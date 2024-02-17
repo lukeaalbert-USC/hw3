6c29f8c541e2c4a36254f50543c52e405efa2554
lukeaalbert-USC <lpalbert@usc.edu>
2024-02-17 15:33:27 -0800

    logic circut sim

--- logicsim/circuit.h
+++ logicsim/circuit.h
@@ -19,11 +19,11 @@ class Circuit
     void endUml(std::ostream&);
 		
 	private:
-		uint64_t m_current_time;
+	uint64_t m_current_time;
     std::vector<Gate*> m_gates;
     std::vector<Wire*> m_wires;
 		// we need to add the m_pq data member. It should be a min-heap of Event*;
-        
+    Heap<Event*, EventLess> m_pq;
 };
 
 #endif
--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -63,13 +63,49 @@ Or2Gate::Or2Gate(Wire* a, Wire* b, Wire* o) : Gate(2,o)
 
 Event* Or2Gate::update(uint64_t current_time)
 {
-    
   char state = '0';
   Event* e = nullptr;
 	for(auto w : m_inputs)
 	{
 		char in = w->getState();
 		if(in == '1')
+		{
+			state = '1';
+			break;
+		}
+		else if(in == 'X')
+		{
+			state = 'X';
+		}
+	}
+  if(state != m_current_state)
+	{
+    m_current_state = state;
+		uint64_t next = current_time + m_delay;
+		e = new Event {next,m_output,state};
+         
+	}
+  return e;
+}
+
+NotGate::NotGate(Wire* in, Wire* out) : Gate(1, out)
+{
+	wireInput(0, in);
+}
+
+Event* NotGate::update(uint64_t current_time)
+{
+  char state;
+  Event* e = nullptr;
+	for(auto w : m_inputs)
+	{
+		char in = w->getState();
+		if(in == '1')
+		{
+			state = '0';
+			break;
+		}
+		else if (in == '0')
 		{
 			state = '1';
 			break;
